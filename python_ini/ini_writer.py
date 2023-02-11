35c54726195a2502c234b330fa9e071b1833ab06
Lowell D. Thomas <ldt@sabnf.com>
2023-02-11 15:25:32 -0500

    Writer working well but no comments yet.

--- python_ini/ini_writer.py
+++ python_ini/ini_writer.py
@@ -20,19 +20,17 @@ sys.path.append(os.getcwd())
 # The grammar object, grammar.py was generated with (assuming PyPI installation of apg-py)
 # python3 apg-py -i python_ini/grammar.abnf
 
-__SECTION = 0
-__KEY = 1
-__COMMENT = 2
-__COMMENT_DELIM_SEMI = '; '
-__COMMENT_DELIM_HASH = '# '
-__COMMENT_TAB = 40
-__KEY_DELIM_EQUALS = ' = '
-__KEY_DELIM_COLON = ': '
-__KEY_DELIM_SPACE = ' '
-__VALUE_DELIM_COMMA = ', '
-__VALUE_DELIM_SPACE = ' '
-__NAME_CHARS = [33, 36, 37, 38, 40, 41, 42, 43, 45,
-                46, 60, 62, 63, 64, 94, 95, 123, 124, 125, 126]
+
+def indent(n):
+    out = ''
+    if(n < 0):
+        n = -n
+    if(n > 120):
+        n = 120
+    while(n):
+        out += ' '
+        n -= 1
+    return out
 
 
 class IniWriter:
@@ -40,12 +38,27 @@ class IniWriter:
     def __init__(self):
         '''Ini file writer constructor.
         '''
+
+        self.__SECTION = 0
+        self.__KEY = 1
+        self.__COMMENT = 2
+        self.__COMMENT_DELIM_SEMI = '; '
+        self.__COMMENT_DELIM_HASH = '# '
+        self.__COMMENT_TAB = 40
+        self.__LINE_END = '\n'
+        self.__KEY_DELIM_EQUALS = ' = '
+        self.__KEY_DELIM_COLON = ': '
+        self.__KEY_DELIM_SPACE = ' '
+        self.__VALUE_DELIM_COMMA = ', '
+        self.__VALUE_DELIM_SPACE = ' '
+        self.__NAME_CHARS = [33, 36, 37, 38, 40, 41, 42, 43, 45,
+                             46, 60, 62, 63, 64, 94, 95, 123, 124, 125, 126]
         self.errors = []
         self.__lines = []
-        self.__comment_delim = __COMMENT_DELIM_SEMI
-        self.__key_delim = __KEY_DELIM_EQUALS
-        self.__value_delim = __VALUE_DELIM_COMMA
-        self.__comment_tab = __COMMENT_TAB
+        self.__comment_delim = self.__COMMENT_DELIM_SEMI
+        self.__key_delim = self.__KEY_DELIM_EQUALS
+        self.__value_delim = self.__VALUE_DELIM_COMMA
+        self.__comment_tab = self.__COMMENT_TAB
 
     def __normalize_comment(self, comment):
         if(comment is None):
@@ -74,14 +87,17 @@ class IniWriter:
                 continue
             if(c >= 97 and c <= 122):
                 continue
-            if(c in __NAME_CHARS):
+            if(c in self.__NAME_CHARS):
                 continue
 
             # invalid character in name
             return False
         return True
 
-    def comment_tab(self, tab=__COMMENT_TAB):
+    def comment_tab(self, tab=None):
+        if(tab is None):
+            self.__comment_tab = self.__COMMENT_TAB
+            return
         if(not isinstance(tab, int) or (int < 0)):
             raise Exception(
                 'IniWriter.comment_tab(): argument positive integer', tab)
@@ -89,29 +105,29 @@ class IniWriter:
 
     def comment_delim(self, delim=';'):
         if(delim == ';'):
-            self.__comment_delim = __COMMENT_DELIM_SEMI
+            self.__comment_delim = self.__COMMENT_DELIM_SEMI
         elif(delim == '#'):
-            self.__comment_delim = __COMMENT_DELIM_HASH
+            self.__comment_delim = self.__COMMENT_DELIM_HASH
         else:
             raise Exception(
                 'IniWriter.comment_delim(): argument must be semicolon(;) or hash(#)', delim)
 
     def key_delim(self, delim='='):
         if(delim == '='):
-            self.__key_delim = __KEY_DELIM_EQUALS
+            self.__key_delim = self.__KEY_DELIM_EQUALS
         elif(delim == ':'):
-            self.__key_delim = __KEY_DELIM_COLON
+            self.__key_delim = self.__KEY_DELIM_COLON
         elif(delim.isspace()):
-            self.__key_delim = __KEY_DELIM_SPACE
+            self.__key_delim = self.__KEY_DELIM_SPACE
         else:
             raise Exception(
                 'IniWriter.key_delim(): argument must be equals(=), comma(,) or space', delim)
 
     def value_delim(self, delim=','):
         if(delim == ','):
-            self.__value_delim = __VALUE_DELIM_COMMA
+            self.__value_delim = self.__VALUE_DELIM_COMMA
         elif(delim.isspace()):
-            self.__value_delim = __VALUE_DELIM_SPACE
+            self.__value_delim = self.__VALUE_DELIM_SPACE
         else:
             raise Exception(
                 'IniWriter.value_delim(): argument must be comma(,) or space', delim)
@@ -119,7 +135,7 @@ class IniWriter:
     def section(self, name, comment=None):
         self.__validate_name(name)
         self.__lines.append(
-            [__SECTION, name, self.__normalize_comment(comment)])
+            [self.__SECTION, name, self.__normalize_comment(comment)])
 
     def __normalize_string(self, string):
         v = ''
@@ -133,8 +149,6 @@ class IniWriter:
                 v += '\\n'
             elif(c == 13):
                 v += '\\r'
-            elif(c == 34):
-                v += '\\"'
             elif(c == 39):
                 v += "\\'"
             elif(c <= 0xff):
@@ -158,27 +172,63 @@ class IniWriter:
             else:
                 raise Exception(
                     'IniWriter: string has invalid characters', string)
-        return v
+        return "'" + v + "'"
 
-    def key(self, name, value, comment=None):
+    def key(self, name, varg, comment=None):
         self.__validate_name(name)
-        if(isinstance(value, int) or isinstance(value, float)):
-            v = value
-        elif(value == True or value == False or value is None):
-            v = value
-        elif(isinstance(value, str)):
-            v = self.__normalize_string(value)
+        if(isinstance(varg, list)):
+            values = varg
         else:
-            raise Exception('IniWriter.key(): invalid value', value)
+            values = [varg]
+        vlist = []
+        for value in values:
+            if(value == True):
+                v = 'true'
+            elif(value == False):
+                v = 'false'
+            elif(value is None):
+                v = 'none'
+            elif(isinstance(value, int) or isinstance(value, float)):
+                v = str(value)
+            elif(isinstance(value, str)):
+                v = self.__normalize_string(value)
+            else:
+                raise Exception('IniWriter.key(): invalid value', value)
+            vlist.append(v)
         self.__lines.append(
-            [__KEY, name, v, self.__normalize_comment(comment)])
+            [self.__KEY, name, vlist, self.__normalize_comment(comment)])
 
-    def comment(self, comment):
-        self.__lines.append([__COMMENT, self.__normalize_comment(comment)])
+    def comment(self, comment=None):
+        self.__lines.append(
+            [self.__COMMENT, self.__normalize_comment(comment)])
 
     def write(self, fname):
         with open(fname, 'r') as fd:
             fd.write(self.to_string())
 
+# key line      = [id, name, value, comment]
+# section line  = [id, name, comment]
+# comment line  = [id, comment]
     def to_string(self):
-        return
+        out = ''
+        for line in self.__lines:
+            if(line[0] == self.__COMMENT):
+                if(line[1] == ''):
+                    out += '\n'
+                else:
+                    if(line[1] is None):
+                        out += self.__LINE_END
+                    else:
+                        out += self.__comment_delim + line[1] + self.__LINE_END
+            elif(line[0] == self.__KEY):
+                out += line[1] + self.__key_delim
+                count = 0
+                for v in line[2]:
+                    if(count > 0):
+                        out += self.__value_delim
+                    out += v
+                    count += 1
+                out += self.__LINE_END
+            else:
+                out += '[' + line[1] + ']' + self.__LINE_END
+        return out
